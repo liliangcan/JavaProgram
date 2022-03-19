dcd49f02c241ed4ea13ad8e1ab064f2c133388fd
liliangcan <91586917+liliangcan@users.noreply.github.com>
2022-03-20 01:40:51 +0800

    Create subsets.java

---
+++ week4/LeetCode/subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                //mask二进制形式中对应位为1，则可以add进t
+                if ((mask & (1 << i)) != 0) {
+                    t.add(nums[i]);
+                }
+            }
+            ans.add(new ArrayList<Integer>(t));
+        }
+        return ans;
+    }
+}
