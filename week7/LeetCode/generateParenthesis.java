576ccdb60222e81175539a6c8d40eac91b5d3091
liliangcan <91586917+liliangcan@users.noreply.github.com>
2022-04-10 20:50:37 +0800

    Create generateParenthesis.java

---
+++ week7/LeetCode/generateParenthesis.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            //设置退出条件
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            //当还能加左括号的时候就加
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            //当有括号数量小于左括号时
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
