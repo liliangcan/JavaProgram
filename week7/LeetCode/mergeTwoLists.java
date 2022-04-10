7e886aaa73bc8fcd2edf4ec3daa8e5287ae0de91
liliangcan <91586917+liliangcan@users.noreply.github.com>
2022-04-10 20:50:17 +0800

    Create mergeTwoLists.java

---
+++ week7/LeetCode/mergeTwoLists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode anshead = new ListNode(-1);
+
+        ListNode tmp = anshead;
+        //当连个链表的current指针都不为空时
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tmp.next = list1;
+                list1 = list1.next;
+            } else {
+                tmp.next = list2;
+                list2 = list2.next;
+            }
+            tmp = tmp.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        tmp.next = list1 == null ? list2 : list1;
+
+        return anshead.next;
+    }
+}
